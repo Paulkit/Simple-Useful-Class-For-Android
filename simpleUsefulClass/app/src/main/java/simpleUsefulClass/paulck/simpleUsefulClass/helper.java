1a464ce7655a91a18d1de2c488e3b81895f0e943
unknown <chungkchu4@XNT14.cs.cityu.edu.hk>
2016-09-23 14:17:31 +0800

    init commit

---
+++ simpleUsefulClass/app/src/main/java/simpleUsefulClass/paulck/simpleUsefulClass/helper.java
@@ -0,0 +1,95 @@
+package androidsugar.paulck.androidsugar;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
+import android.os.SystemClock;
+import android.view.ContextThemeWrapper;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by paulck on 9/2/2016.
+ */
+public class helper {
+    private Context mContext;
+
+    private long mLastClickTime = 0;
+
+    helper(Context context) {
+        mContext = context;
+    }
+
+
+    public void storePrefrence(String[] tag, String[] val) {
+        SharedPreferences.Editor editor = mContext.getSharedPreferences(mContext.getPackageName(), Context.MODE_PRIVATE).edit();
+        for (int i = 0; i < tag.length; i++) {
+            editor.putString(tag[i], val[i]).apply();
+        }
+    }
+    public String[] getPrefrence(String[] tag) {
+        SharedPreferences  editor = mContext.getSharedPreferences(mContext.getPackageName(), Context.MODE_PRIVATE);
+        for (int i = 0; i < tag.length; i++) {
+            tag[i] = editor.getString(tag[i],null);
+        }
+        return tag;
+    }
+
+    public void clearPrefrence() {
+        SharedPreferences.Editor editor = mContext.getSharedPreferences(mContext.getPackageName(), Context.MODE_PRIVATE).edit();
+        editor.clear().apply();
+    }
+
+    public void removePrefrence(String[] tag) {
+        SharedPreferences.Editor editor = mContext.getSharedPreferences(mContext.getPackageName(), Context.MODE_PRIVATE).edit();
+
+        for (int i = 0; i < tag.length; i++) {
+            editor.remove(tag[i]).apply();
+        }
+    }
+
+    public static void stateControls(boolean able, ViewGroup vg) {
+        for (int i = 0; i < vg.getChildCount(); i++) {
+            View child = vg.getChildAt(i);
+            child.setEnabled(able);
+            if (child instanceof ViewGroup) {
+                stateControls(able, (ViewGroup) child);
+            }
+        }
+    }
+
+    public boolean misClickCheck() {
+        // mis-clicking prevention, using threshold of 1000 ms
+        if (SystemClock.elapsedRealtime() - mLastClickTime < 1000) {
+            return false;
+        }
+        mLastClickTime = SystemClock.elapsedRealtime();
+        return true;
+    }
+
+    public void alertBuilderConfirm(String title, String msg, String yes, DialogInterface.OnClickListener yesListen) {
+        AlertDialog.Builder alert = new AlertDialog.Builder(new ContextThemeWrapper(mContext, R.style.AppThemeNoActionBar));
+        alert.setTitle(title);
+        alert.setMessage(msg);
+        alert.setCancelable(false);
+        alert.setPositiveButton(yes, yesListen);
+
+        alert.show();
+    }
+
+    public void alertBuilder(String title, String msg, String yes, String no, DialogInterface.OnClickListener yesListen, DialogInterface.OnClickListener noListen) {
+        AlertDialog.Builder alert = new AlertDialog.Builder(new ContextThemeWrapper(mContext, R.style.AppThemeNoActionBar));
+        alert.setTitle(title);
+        alert.setMessage(msg);
+        alert.setCancelable(false);
+        if (yes != null) {
+            alert.setPositiveButton(yes, yesListen);
+        }
+        if (no != null) {
+            alert.setNegativeButton(no, noListen);
+        }
+        alert.show();
+    }
+
+}
